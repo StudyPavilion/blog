7ec5443b18efd1bf87786333211ccc04f85e8249
StudyPavilion <75730441+StudyPavilion@users.noreply.github.com>
2024-04-14 16:14:15 +0000

    Deploying to gh-pages from @ StudyPavilion/blog@b8705bd79b1af8c6a2f1f5e911b5c9bc6964ec8c 🚀

---
+++ assets/index.html-DCM7Sy6I.js
@@ -0,0 +1,25 @@
+import{_ as c}from"./plugin-vue_export-helper-DlAUqK2U.js";import{r as a,o as s,c as d,b as r,a as e,d as t,w as p,e as n}from"./app-ix-xb7Xz.js";const h={},m=e("p",null,"此处是 Mr.Hope 存放的一些代码笔记。",-1),u=e("h2",{id:"目录",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#目录"},[e("span",null,"目录")])],-1),_=e("h3",{id:"代码学习",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#代码学习"},[e("span",null,"代码学习")])],-1),g=e("h3",{id:"代码学习网站",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#代码学习网站"},[e("span",null,"代码学习网站")])],-1),f=e("h3",{id:"优质网站",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#优质网站"},[e("span",null,"优质网站")])],-1),w={href:"https://wangdoc.com/",target:"_blank",rel:"noopener noreferrer"},E={href:"https://www.liaoxuefeng.com/",target:"_blank",rel:"noopener noreferrer"},b={href:"https://developer.mozilla.org/zh-CN/",target:"_blank",rel:"noopener noreferrer"},x=e("div",{class:"hint-container info"},[e("p",{class:"hint-container-title"},"相关信息"),e("p",null,"MDN 的中文文档翻译很差，大多都是机翻，十分建议直接阅读英文文档。")],-1),y=e("h3",{id:"杂牌网站",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#杂牌网站"},[e("span",null,"杂牌网站")])],-1),k={href:"https://www.runoob.com/",target:"_blank",rel:"noopener noreferrer"},M={href:"https://www.w3cschool.cn",target:"_blank",rel:"noopener noreferrer"},A={href:"http://www.w3school.com.cn/",target:"_blank",rel:"noopener noreferrer"};function D(R,v){const i=a("RouteLink"),o=a("ExternalLinkIcon"),l=a("Badge");return s(),d("div",null,[m,r(" more "),u,r(` ### 基础
+
+- [基础知识](basic/README.md)
+
+- [Windows 基础](windows/README.md)
+
+- [GitHub 介绍](github/README.md) `),_,e("ul",null,[e("li",null,[t(i,{to:"/code/language/"},{default:p(()=>[n("代码学习")]),_:1})])]),r(` - [如何学习一门语言](language/learning.md)
+
+- [JavaScript 教程](language/js/README.md)
+
+- [TypeScript 教程](language/typescript/README.md)
+
+- [Python 教程](language/python/README.md) (编写中...)
+
+- [Markdown 教程](language/markdown/README.md)
+
+- [YAML 教程](language/yaml/README.md)
+
+- [JSON 教程](language/json/README.md) `),r(` ### APP
+
+- [Android 开发](android/README.md)
+
+### 后端开发
+
+- [后端开发](back-end/README.md) `),g,f,e("ul",null,[e("li",null,[e("p",null,[e("a",w,[n("网道"),t(o)])])]),e("li",null,[e("p",null,[e("a",E,[n("廖雪峰的网站"),t(o)])])]),e("li",null,[e("p",null,[e("a",b,[n("MDN"),t(o)])]),x])]),y,e("ul",null,[e("li",null,[e("p",null,[e("a",k,[n("菜鸟教程"),t(o)]),n(),t(l,{text:"内容比较新"})])]),e("li",null,[e("p",null,[e("a",M,[n("w3cschool"),t(o)]),n(),t(l,{text:"内容最新"}),n(),t(l,{text:"有手机APP"}),n(),t(l,{text:"广告信息多",type:"warn"})])]),e("li",null,[e("p",null,[e("a",A,[n("w3school"),t(o)]),n(),t(l,{text:"内容比较旧",type:"warn"})])])])])}const P=c(h,[["render",D],["__file","index.html.vue"]]),C=JSON.parse('{"path":"/code/","title":"代码笔记","lang":"zh-CN","frontmatter":{"date":"2022-01-07T00:00:00.000Z","title":"代码笔记","icon":"code","star":true,"sticky":20,"next":"./basic/","description":"此处是 Mr.Hope 存放的一些代码笔记。","head":[["meta",{"property":"og:url","content":"https://mister-hope.github.io/blog/code/"}],["meta",{"property":"og:site_name","content":"学习阁"}],["meta",{"property":"og:title","content":"代码笔记"}],["meta",{"property":"og:description","content":"此处是 Mr.Hope 存放的一些代码笔记。"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2024-04-14T16:12:41.000Z"}],["meta",{"property":"article:author","content":"学习阁"}],["meta",{"property":"article:published_time","content":"2022-01-07T00:00:00.000Z"}],["meta",{"property":"article:modified_time","content":"2024-04-14T16:12:41.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"代码笔记\\",\\"image\\":[\\"\\"],\\"datePublished\\":\\"2022-01-07T00:00:00.000Z\\",\\"dateModified\\":\\"2024-04-14T16:12:41.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"学习阁\\",\\"url\\":\\"https://space.bilibili.com/636126449?spm_id_from=333.1007.0.0\\"}]}"]]},"headers":[{"level":2,"title":"目录","slug":"目录","link":"#目录","children":[{"level":3,"title":"代码学习","slug":"代码学习","link":"#代码学习","children":[]},{"level":3,"title":"代码学习网站","slug":"代码学习网站","link":"#代码学习网站","children":[]},{"level":3,"title":"优质网站","slug":"优质网站","link":"#优质网站","children":[]},{"level":3,"title":"杂牌网站","slug":"杂牌网站","link":"#杂牌网站","children":[]}]}],"git":{"createdTime":1713111161000,"updatedTime":1713111161000,"contributors":[{"name":"StudyPavilion","email":"1475109413@qq.com","commits":1}]},"readingTime":{"minutes":0.74,"words":222},"filePathRelative":"code/README.md","localizedDate":"2022年1月7日","excerpt":"<p>此处是 Mr.Hope 存放的一些代码笔记。</p>\\n","autoDesc":true}');export{P as comp,C as data};
